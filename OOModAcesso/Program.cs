215e06b1d377f90a64506816b6bdeb38d74ff55e
Hiago Grades <hiagogrades@hotmail.com>
2023-01-23 15:11:41 -0300

    Finaliza Modificadores de acesso

--- OOModAcesso/Program.cs
+++ OOModAcesso/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OOModAcesso
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var payment = new Payment();
+
             
+            
+        }
+    }
+
+    public class Payment
+    {
+        protected DateTime Vencimento;
+        
+        void Pagar()
+        {
+
+        }
+    }
+
+    class PaymentTicket : Payment
+    {
+        public DateTime Vencimento;
+
+        void Pagar()
+        {
+
+        }
+
+        void Test()
+        {
+            //Utilizando o base para acessar as propriedades da classe 
+            base.Vencimento = DateTime.Now;
         }
     }
 }
