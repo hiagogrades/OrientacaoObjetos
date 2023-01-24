7064245c87efd8f36a11440e404d5777c4916b96
Hiago Grades <hiagogrades@hotmail.com>
2023-01-24 09:07:43 -0300

    Inicia projeto para pratica de Interfaces

---
+++ OOInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+        }
+    }
+}
