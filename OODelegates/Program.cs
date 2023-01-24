e795c404aca62807485b4f41033fdf57604c0627
Hiago Grades <hiagogrades@hotmail.com>
2023-01-24 14:04:52 -0300

    Finaliza Delegates

--- OODelegates/Program.cs
+++ OODelegates/Program.cs
@@ -4,9 +4,30 @@ namespace OODelegates
 {
     internal class Program
     {
+        static void RealizarPagamento(double valor)
+        {
+            Console.WriteLine($"Pago o valor de {valor}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pagar = new Pagamento.Pagar(RealizarPagamento);
+
+            //Envoca a função delegada
+            pagar(25);
         }
+    }
+
+    //Implementando o delegate
+    public class Pagamento
+    {
+        //Definindo o construtor de maneira "padrão", sem o delegate
+        //public void Pagar(double valor)
+        //{
+
+        //}
+
+        //Utilizando o delagete para fazer o construtor em "outro lugar"
+        public delegate void Pagar(double valor);
     }
 }
