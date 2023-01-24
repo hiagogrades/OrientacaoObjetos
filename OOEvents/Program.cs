aed71a7ad46cb5781403b96f93fdb34938660a95
Hiago Grades <hiagogrades@hotmail.com>
2023-01-24 16:19:21 -0300

    Finaliza Events

---
+++ OOEvents/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace OOEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            var room = new Room(3);
+            room.RoomSoldOutEvent += OnRoomSoldOut;
+
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+        }
+
+        static void OnRoomSoldOut(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sala lotada");
+        }
+    }
+
+    public class Room
+    {
+        public Room(int seats)
+        {
+            Seats = seats;
+            seatsInUse = 0;
+        }
+
+        private int seatsInUse = 0;
+
+        public int Seats { get; set; }
+
+        public void ReserveSeat()
+        {
+            seatsInUse++;
+            if (seatsInUse >= Seats)
+            {
+                //Evento fechado
+                OnRoomSoldOut(EventArgs.Empty);
+            }
+            else
+            {
+                Console.WriteLine("Assento reservado");
+            }
+        }
+
+        //cria uma definição para o evento
+        public event EventHandler RoomSoldOutEvent;
+
+        //Impementação base para a execução do evento
+        protected virtual void OnRoomSoldOut(EventArgs e)
+        {
+            EventHandler handler = RoomSoldOutEvent;
+            handler?.Invoke(this, e);
+        }
+    }
+}
