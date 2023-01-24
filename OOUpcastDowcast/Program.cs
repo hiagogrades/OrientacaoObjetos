c149ce738272a16618e9ca1a2bb6ec08ee3272d6
Hiago Grades <hiagogrades@hotmail.com>
2023-01-24 11:45:57 -0300

    Finaliza Upcast e DownCast

---
+++ OOUpcastDowcast/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOUpcastDowcast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Upcast
+            var pessoa = new Person();
+            var pessoaFisica = new Personal();
+            var pessoaJuridica = new Corporate();
+
+            var
+
+            //Downcast
+            pessoaFisica = (Personal)pessoa; //Realiza uma conversão de tipo para Personal
+
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Personal : Person 
+    {
+        public string CIC { get; set; }
+    }
+
+    public class Corporate : Person
+    {
+        public string CGC { get; set; }
+    }
+}
