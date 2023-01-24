5fc100e980bde988fee7c24a693085f4c6ebee29
Hiago Grades <hiagogrades@hotmail.com>
2023-01-23 21:16:06 -0300

    Finaliza os exemplos com using e Dispose

--- OOUsingDispose/OOUsingDispose/Program.cs
+++ OOUsingDispose/OOUsingDispose/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.WebSockets;
 
 namespace OOUsingDispose
 {
@@ -6,7 +7,31 @@ namespace OOUsingDispose
     {
         static void Main(string[] args)
         {
-            
+            //Dispose manual
+            //var payment = new Payment();
+            //payment.Dispose(); //Chamando o método Dispose para destruir a classe Payment
+
+            //Forma automática de utilizar o garbage colletor
+            using (var payment = new Payment())
+            {
+                Console.WriteLine("Processando pagamento.");
+            }
+        }
+
+        public class Payment : IDisposable
+        {
+            //Garbage Colletor -> gc.collect
+
+            //Define o Método construtor
+            public Payment()
+            {
+                Console.WriteLine("Iniciando o pagamento.");
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("Finalizando o pagamento.");
+            }
         }
     }
 }
