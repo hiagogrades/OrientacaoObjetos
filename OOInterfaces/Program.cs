63fa18c89ef852593852409732b2205d792f15ed
Hiago Grades <hiagogrades@hotmail.com>
2023-01-24 09:40:39 -0300

    Finaliza os exemplos de Interface

--- OOInterfaces/Program.cs
+++ OOInterfaces/Program.cs
@@ -9,4 +9,24 @@ namespace OOInterfaces
             Console.Clear();
         }
     }
+
+    public class Payment : IPayment
+    {
+        public DateTime Vencimento { get; set; }
+
+        public void Pagar (double valor)
+        {
+
+        }
+    }
+
+    public interface IPayment
+    {
+        DateTime Vencimento { get; set; }
+
+        void Pagar (double valor)
+        {
+
+        }
+    }
 }
