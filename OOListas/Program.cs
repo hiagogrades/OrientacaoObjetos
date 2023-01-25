f2311460ac2f9aee067c571c6d7e6849dbfb9fa6
Hiago Grades <hiagogrades@hotmail.com>
2023-01-25 11:54:57 -0300

    Finaliza Listas - Parte 1

---
+++ OOListas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var payments = new List<Payment>();
+            IList<Payment> payments = new List<Payment>();
+            payments.Add(new Payment());
+            payments.Remove(new Payment());
+        }
+    }
+
+    public class Payment
+    {
+
+    }
+}
