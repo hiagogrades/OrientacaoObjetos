2df93860195c5a25dbb05e595ef8f3090a775c8d
Hiago Grades <hiagogrades@hotmail.com>
2023-01-25 17:54:08 -0300

    Inicia o projeto Mão na Massa. Balta-io

---
+++ Balta/Program.cs
@@ -0,0 +1,14 @@
+﻿using Balta.ContentContext;
+using System;
+
+namespace Balta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+            
+        }
+    }
+}
