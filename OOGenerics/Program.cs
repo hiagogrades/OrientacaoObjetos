5678e225c807972f97f0f78a03344804adf07416
Hiago Grades <hiagogrades@hotmail.com>
2023-01-25 10:20:46 -0300

    Finaliza Generics - Parte 2

--- OOGenerics/Program.cs
+++ OOGenerics/Program.cs
@@ -5,23 +5,56 @@ namespace OOGenerics
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    var context = new DataContext<Person>();
+        //    var person = new Person();
+        //    context.Save(person); //Salvando uma pessoa
+        //}
+
         static void Main(string[] args)
         {
-            var context = new DataContext<Person>();
             var person = new Person();
-            context.Save(person); //Salvando uma pessoa
+            var payment = new Payment();
+            var subscription = new Subscription();
+            var context = new DataContext<Person, Payment, Subscription>();
+
+            context.Save(person);
+            context.Save(payment);
+            context.Save(subscription);
         }
     }
 
     //Para dizer que uma classe é genérica em C#, utilize os sinais (<>);
     //Como por exemplo <TipoDaClasse>
     //DataContext<T>
-    public class DataContext<T>
+    //public class DataContext<T>
+    //{
+    //    //Método para salvar as informações
+    //    public void Save(T entity)
+    //    {
+    //    }
+    //}
+
+    //Mais de um tipo genérico
+    //Where => utilizar para explicitar suas respectivas ordens e letras
+    public class DataContext<P, PA, S>
+        where P : Person,
+        where PA: Payment,
+        where S : Subscription
     {
-        //Método para salvar as informações
-        public void Save(T entity)
+        public void Save(P entity)
         {
-            
+
+        }
+
+        public void Save(PA entity)
+        {
+
+        }
+
+        public void Save(S entity)
+        {
 
         }
     }
